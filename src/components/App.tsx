d8122cc7f5bf543c9dc40e112d6c825d12e2dca5
ditole <tolerevo@hotmail.com>
2024-04-15 07:47:49 -0600

    furst commmit

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import "../App.css";
+import { Heading } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+     <Heading as='h1'>
+      Chakra Ui is simple blah blah blah...
+     </Heading>
+    </div>
+  );
+}
+
+export default App;
